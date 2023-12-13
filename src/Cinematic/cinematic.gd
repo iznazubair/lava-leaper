7cb7170c1ceeacb98c6cddc8eedd9ebb38468acb
Raiyaan17 <raiyaanjhamid@gmail.com>
2023-12-14 02:18:17 +0500

    troubleshooting

--- src/Cinematic/cinematic.gd
+++ src/Cinematic/cinematic.gd
@@ -5,3 +5,7 @@ func _ready():
 
 func _on_animation_player_animation_finished(anim_name):
 	get_tree().change_scene_to_file("res://MenuScreens/main_menu.tscn")
+
+func _physics_process(delta):
+	if(Input.is_anything_pressed()):
+		get_tree().change_scene_to_file("res://MenuScreens/main_menu.tscn")
